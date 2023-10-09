d42e0f9fbdf865db4885ce86f33b37d357fb26d1
Emyllebsousa <emyllebeatrizdesousa@gmail.com>
2023-10-09 16:14:11 -0300

    conferir

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+ 
+  System.out.println("Menu/n 1-saldo /n");
+  System.out.println(" digite sua opção: ");
+   Scanner scan = new Scanner(System.in);
+  int opcao= scan.nextInt();
+switch(opcao){
+case 1:
+ Conta.Versaldo();
+break;
+}
+
     }
 }
