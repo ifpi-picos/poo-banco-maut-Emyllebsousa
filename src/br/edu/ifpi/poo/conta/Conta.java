84dc4fba71d2ae6d804d4b468ef2d5d14b7df949
Emyllebsousa <emyllebeatrizdesousa@gmail.com>
2023-10-23 16:58:33 -0300

    organizando conta

--- src/br/edu/ifpi/poo/conta/Conta.java
+++ src/br/edu/ifpi/poo/conta/Conta.java
@@ -1,28 +1,22 @@
 package br.edu.ifpi.poo.conta;
-
-
-
 import br.edu.ifpi.poo.cliente.Cliente;
 
 public abstract class Conta {
 
-
-
     protected double saldo;
     private String numero;
-    
     private String numeroag;
     private Cliente cliente;
-
-
 
     public Conta(double saldo, String numero, String numeroag, Cliente cliente) {
         this.saldo = saldo;
         this.numero = numero;
-         this.numeroag = numeroag;
+        this.numeroag = numeroag;
         this.cliente = cliente;
         
     }
+
+    //----------------------------------------------------------------------------------------------------
     public abstract void transfere(double valor, Conta destino);
 
     public void saque(double valor){
@@ -32,13 +26,19 @@ public abstract class Conta {
     this.saldo+= valor;
     
     }
+
+    //-----------------------------------------------------------------------------------------------
     public double getSaldo() {
         return saldo;
     }
-
-public String getNumero() {
-        return numero;
-    }
-
+    public String getNumeroag() {
+            return numeroag;
+        }
+    public String getNumero() {
+            return numero;
+        }
+    public Cliente getCliente() {
+            return cliente;
+        }
 
 }
