a0ab0337b9a4829a29ebfe852492a71341d0c847
Emyllebsousa <emyllebeatrizdesousa@gmail.com>
2023-10-23 16:50:50 -0300

    erro notificacao

--- src/br/edu/ifpi/poo/conta/Conta.java
+++ src/br/edu/ifpi/poo/conta/Conta.java
@@ -1,6 +1,6 @@
 package br.edu.ifpi.poo.conta;
 
-import javax.management.Notification;
+
 
 import br.edu.ifpi.poo.cliente.Cliente;
 
@@ -11,7 +11,11 @@ public abstract class Conta {
     protected double saldo;
     private String numero;
     private String numeroag;
-    private Notification notificacao;
+    
+   
+        
+
+
     private Cliente cliente;
 
 
@@ -21,10 +25,12 @@ public abstract class Conta {
         this.numero = numero;
          this.numeroag = numeroag;
         this.cliente = cliente;
+        
     }
+    public abstract void transfere(double valor, Conta destino);
+
     public void saque(double valor){
         this.saldo -=  valor;
-        this.notificacao.enviaNotificacao("saque", valor);
     }
 
     public double getSaldo() {
