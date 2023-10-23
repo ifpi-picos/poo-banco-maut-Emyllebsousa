374e9fcefd5853b314f35befbe0014e085d07ea8
Emyllebsousa <emyllebeatrizdesousa@gmail.com>
2023-10-23 17:07:21 -0300

    classe cliente organizando

--- src/br/edu/ifpi/poo/cliente/Cliente.java
+++ src/br/edu/ifpi/poo/cliente/Cliente.java
@@ -1,4 +1,34 @@
 package br.edu.ifpi.poo.cliente;
+
+import br.edu.ifpi.poo.endereco.Endereco;
+
 public class Cliente {
-    
+
+    private String nome;
+    private String cpf;
+    private String dtn;
+    private Endereco endereco;
+
+ 
+    public Cliente(String nome, String cpf, String dtn, Endereco endereco) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dtn = dtn;
+        this.endereco = endereco;
+    }
+
+
+    //-----------------------------------------------------------------------------------------
+    public String getNome() {
+        return nome;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+     public String getDtn() {
+        return dtn;
+    }
+        public Endereco getEndereco() {
+        return endereco;
+    }
 }
