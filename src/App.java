01bf667c7cc06f7999355540430dda096878ddc7
Emyllebsousa <emyllebeatrizdesousa@gmail.com>
2023-10-25 13:52:49 -0300

    get saldo main

--- src/App.java
+++ src/App.java
@@ -2,16 +2,19 @@ import br.edu.ifpi.poo.cliente.Cliente;
 import br.edu.ifpi.poo.conta.Conta;
 import br.edu.ifpi.poo.conta.Contacorrente;
 import br.edu.ifpi.poo.conta.Contapoupanca;
+import br.edu.ifpi.poo.endereco.Endereco;
+import br.edu.ifpi.poo.notificacao.NotificacaoEmail;
+import br.edu.ifpi.poo.notificacao.NotificacaoSms;
 
 
 
 public class App {
+
     public static void main(String[] args) throws Exception {
-        Cliente A= new Cliente(null, null, null, null);
-        Conta conta1 = new Contacorrente(50, "001","111", A,null, 0);
-        Conta conta2 = new Contapoupanca(0, "002", "222", A, null);
-       
-    conta1.getSaldo();
-    conta2.getSaldo();
+    Cliente A= new Cliente("Emy", "000.000.000-01", "08/03/2005", new Endereco("Rua das Flores", "Centro", "Dom Expedito", 55));
+        Conta conta1 = new Contacorrente(50, "001","111", A,new NotificacaoEmail(), 0);
+        Conta conta2 = new Contapoupanca(0, "002", "222", A, new NotificacaoSms());
+       System.out.println("saldo" + conta1.getSaldo());
+       System.out.println("saldo" + conta2.getSaldo());
     }
 }
