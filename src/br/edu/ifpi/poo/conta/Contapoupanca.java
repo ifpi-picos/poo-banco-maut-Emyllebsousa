8c3758ae1afd91b6f29518b340182fe6b1dd0c44
Emyllebsousa <emyllebeatrizdesousa@gmail.com>
2023-10-25 15:45:21 -0300

    notificações de poupança

--- src/br/edu/ifpi/poo/conta/Contapoupanca.java
+++ src/br/edu/ifpi/poo/conta/Contapoupanca.java
@@ -9,10 +9,6 @@ private double taxa;
 private double taxasaque;
 private double rendimento;
 
- 
-
-
-
 
     public Contapoupanca(String numero, String numeroag, Cliente cliente, Notificacao notificacao) {
     super(numero, numeroag, cliente, notificacao);
@@ -21,40 +17,59 @@ private double rendimento;
     this.rendimento=0.1;
   }
 
-    
+  //---------------------------------------------------------------------------------------------------------------  
 
     @Override
     public void transfere(double valor, Conta destino) {
-      sacar(valor);
-      this.saldo =- valor * taxa;
-      destino.deposito(valor);
       
+      sacar(valor);
+
+      this.saldo =- valor * taxa;
+
+      destino.deposito(valor);
+
+      //-------------------------------------------------------------
+      this.notificacao.enviaNotificacao("transação: ", valor);
+      addHistoricoTransacao(valor, "transação");
+      //--------------------------------------------------------------
     }
 
     @Override
     public void sacar(double valor) {
+
       this.saldo-= valor +(valor*taxasaque);
 
-      System.out.println(getSaldo());
+      //-----------------------------------------------------------------
+      this.notificacao.enviaNotificacao("Saque", valor);
+      addHistoricoTransacao(valor, "saque");
+      //------------------------------------------------------------------
     }
 
     @Override
     public void deposito(double valor) {
+
       if(valor>0){
-        this.saldo += (valor + (valor * 0.1));
-        this.notificacao.enviaNotificacao("deposito", valor);
+      this.saldo += (valor + (valor * 0.1));
+
+      //-----------------------------------------------------------------------
+      this.notificacao.enviaNotificacao("deposito", valor);
       addHistoricoTransacao(valor, "deposito");
+      //------------------------------------------------------------------------
       }else{
-        System.out.println("Deposito nao realizado!!");
+
+      System.out.println("ESTE DEPOSITO NÃO PODE SER REALIZADO!");
+
       }
     }
-public double getTaxa() {
-  return taxa;
-}
-public double getTaxasaque() {
-  return taxasaque;
-}
-public double getRendimento() {
-  return rendimento;
-}
+
+    //---------------------------------------------------------------------------------------------------------
+  public double getTaxa() {
+    return taxa;
+  }
+  public double getTaxasaque() {
+    return taxasaque;
+  }
+  public double getRendimento() {
+    return rendimento;
+  }
 }
