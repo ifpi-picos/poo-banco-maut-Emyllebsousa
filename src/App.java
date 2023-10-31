41ea79154b334e99a8b2a08d28736090d0159442
Jesiel Viana <jesielviana@proton.me>
2023-10-31 14:29:40 -0300

    Feedback sobre a atividade

--- src/App.java
+++ src/App.java
@@ -11,21 +11,32 @@ import br.edu.ifpi.poo.notificacao.NotificacaoSms;
 public class App {
 
     public static void main(String[] args) throws Exception {
-    Cliente A= new Cliente("Emy", "000.000.000-01", "08/03/2005", new Endereco("Rua das Flores", "Centro", "Dom Expedito", 55));
-        Conta conta1 = new Contacorrente("001","111", A,new NotificacaoEmail(), 0);
-        Conta conta2 = new Contapoupanca("002", "222", A, new NotificacaoSms());
+        Cliente A = new Cliente("Emy", "000.000.000-01", "08/03/2005",
+                new Endereco("Rua das Flores", "Centro", "Dom Expedito", 55));
+        Conta cc = new Contacorrente("001", "111", A, new NotificacaoEmail(), 0.1);
+        Conta cc2 = new Contacorrente("001", "111", A, new NotificacaoEmail(), 0);
+        Conta cp = new Contapoupanca("002", "222", A, new NotificacaoSms());
 
-conta1.deposito(150);
-conta1.transfere(10, conta2);
-System.out.println(conta1.getSaldo());
-conta1.transfere(10, conta2);
-System.out.println(conta1.getSaldo());
-conta1.transfere(10, conta2);
-System.out.println(conta1.getSaldo());
-conta1.transfere(10, conta2);
-System.out.println(conta1.getSaldo());
+        cc.deposito(100);
+        System.out.println(cc.getSaldo());
+        cc.sacar(50);
+        System.out.println(cc.getSaldo());
+        cc.transfere(10, cc2);
+        cc.transfere(10, cc2);
+        cc.transfere(10, cc2);
+        cc.transfere(10, cc2);
+        cc.transfere(10, cc2);
+        cc.transfere(10, cc2);
+        System.out.println(cc.getSaldo());
+        cc.exibeExtrato();
 
+        cp.deposito(100);
+        cp.sacar(10);
 
-      
+        System.out.println(cp.getSaldo());
+        cp.exibeExtrato();
+
+        // parabéns, tudo certo
+
     }
 }
